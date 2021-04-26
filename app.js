2f2405cf164d063b16eb0740ac816a98ea8aba32
lettuce <lettuce.lu@lettucelu.com>
2021-04-26 12:19:23 +0800

    post 測試

--- app.js
+++ app.js
@@ -21,10 +21,15 @@ app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
 
-  // console.log('get request at /')
+  console.log(req.query)
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  console.log(`收到 post 請求`)
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`server is running at http://localhost:${port}`)
